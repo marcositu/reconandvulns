96f19b3a4cdce9685cecd1554cfcb9c64716bd25
Marcos <marcositu@gmail.com>
2020-06-21 02:26:24 -0300

    Update onlydomain.sh

--- solodominio_en/onlydomain.sh
+++ solodominio_en/onlydomain.sh
@@ -18,7 +18,7 @@ if [[ -z $1 ]]; then
 	mkdir -p ${MYDIR}/${DOMAIN}/assetfinder
 	mkdir -p ${MYDIR}/${DOMAIN}/sublist3r
 	mkdir -p ${MYDIR}/${DOMAIN}/knockpy
-	mkdir -p ${MYDIR}/${DOMAIN}/aquatone
+	mkdir -p ${MYDIR}/${DOMAIN}/eyewitness
 	mkdir -p ${MYDIR}/${DOMAIN}/naabu
 	mkdir -p ${MYDIR}/${DOMAIN}/findomain
 	mkdir -p ${MYDIR}/${DOMAIN}/shodan
@@ -64,12 +64,16 @@ if [[ -z $1 ]]; then
 	}  
 
 	function_merge () {
+		echo "$(tput setab 1) [-] merge$(tput sgr 0)"
 		cd ${MYDIR}/${DOMAIN}/
 		cat ${MYDIR}/${DOMAIN}/sublist3r/sublist3r_${DOMAIN}.txt >> ${MYDIR}/${DOMAIN}/${DOMAIN}.txt
 		cat ${MYDIR}/${DOMAIN}/assetfinder/assetfinder_${DOMAIN}.txt >> ${MYDIR}/${DOMAIN}/${DOMAIN}.txt
 		cat ${MYDIR}/${DOMAIN}/findomain/${DOMAIN}.txt >> ${MYDIR}/${DOMAIN}/${DOMAIN}.txt
-		sort ${MYDIR}/${DOMAIN}/${DOMAIN}.txt | sed 's/[[:blank:]]//g' | uniq >> ${MYDIR}/${DOMAIN}/${DOMAIN}_pre_final.txt
-		sort ${MYDIR}/${DOMAIN}/${DOMAIN}_pre_final.txt | uniq >> ${MYDIR}/${DOMAIN}/${DOMAIN}_final.txt
+		sort ${MYDIR}/${DOMAIN}/${DOMAIN}.txt | sed 's/[[:blank:]]//g' | uniq >> ${MYDIR}/${DOMAIN}/${DOMAIN}_pre1_final.txt
+		cat ${MYDIR}/${DOMAIN}/${DOMAIN}_pre1_final.txt | fprobe -p xlarge >> ${MIDIR}/${DOMINIO}/${DOMINIO}_pre2_final.txt
+		sort -u ${MYDIR}/${DOMAIN}/${DOMAIN}_pre2_final.txt >> ${MYDIR}/${DOMAIN}/${DOMAIN}_final.txt
+		echo "$(tput setab 2)   [-] [OK]$(tput sgr 0)"
+
 	}
 
 	function_knockpy () {
@@ -87,22 +91,22 @@ if [[ -z $1 ]]; then
 		echo "$(tput setab 2)   [-] [OK]$(tput sgr 0)"
 	} 
 
-	function_aquatone () {
-		echo "$(tput setab 1) [-] aquatone$(tput sgr 0)"
-		cd ${MYDIR}/${DOMAIN}/aquatone
-		cat ${MYDIR}/${DOMAIN}/${DOMAIN}_final.txt | aquatone -scan-timeout 4000 -ports 9002,27015,8443,8843,60443,10443,4443,80,81,300,443,591,593,832,981,1010,1311,2082,2087,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5800,6543,7000,7396,7474,8000,8001,8008,8014,8042,8069,8080,8081,8088,8090,8091,8118,8123,8172,8222,8243,8280,8281,8333,8443,8500,8834,8880,8888,8983,9000,9043,9060,9080,9090,9091,9200,9443,9800,9981,12443,16080,18091,18092,20720,28017 -silent
+	function_eyewitness () {
+		echo "$(tput setab 1) [-] eyewitness$(tput sgr 0)"
+		cd ${MYDIR}/${DOMAIN}/eyewitness
+		python3 ~/tools/EyeWitness/Python/EyeWitness.py --web --timeout 20 --delay 3 --threads 2 -f ${MYDIR}/${DOMAIN}/${DOMAIN}_final.txt --no-prompt -d screens >/dev/null 2>/dev/null
 		echo "$(tput setab 2)   [-] [OK]$(tput sgr 0)"
 	}
 
 	function_shodan () {
 		echo "$(tput setab 1) [-] shodan$(tput sgr 0)"
-		capacidad0=$(wc -c <"${MYDIR}/${DOMAIN}/aquatone/aquatone_urls.txt")
+		capacidad0=$(wc -c <"${MYDIR}/${DOMAIN}/eyewitness/screens/open_ports.csv")
 		if [ $capacidad0 == 0 ]; then
 				echo "$(tput setab 5)   [-] [Aquatone without IPs]$(tput sgr 0)"
 		else
-		cd ${MYDIR}/${DOMAIN}/aquatone
+		cd ${MYDIR}/${DOMAIN}/shodan
 
-		for i in $(awk -F/ '{print $3}' aquatone_urls.txt  | sort -u | xargs -l dig -t a +short | sort  -u | cf-check) ; do
+		for i in $(awk -F/ '{print $3}' ${MYDIR}/${DOMAIN}/eyewitness/screens/open_ports.csv | cut -d : -f1 | cut -d , -f1 |  sed '/^$/d' | sort -u | xargs -l dig -t a +short | sort  -u | cf-check) ; do
 		python3 ~/tools/Shodanfy.py/shodanfy.py ${i} --getvuln --getports --getinfo --getbanner >> ${MYDIR}/${DOMAIN}/shodan/${i}.txt 2>/dev/null; done
 		echo "$(tput setab 2)   [-] [OK]$(tput sgr 0)"
 		fi
@@ -121,7 +125,7 @@ if [[ -z $1 ]]; then
 	function_findomain
 	function_merge
 	#function_naabu
-	function_aquatone
+	function_eyewitness
 	function_shodan
 	function_notifications_end
 fi
